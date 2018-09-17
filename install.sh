4765130a9455cc7fce74c070e25d6f185c6b3086
neko <le.neko@gmail.com>
2018-09-17 23:27:01 +0200

    vscode extension sync

--- install.sh
+++ install.sh
@@ -5,23 +5,46 @@ export DIR
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # bash
-function install_bash(){
-    for f in $(fd . bash); do
+function bash_cfg(){
+    for f in $(fd . -t f bash); do
         in="$DIR/$f"
-        out="~/.${f##*/}"
-        ln -sfv "$in $out"
+        out=$(echo ~/.${f##*/})
+        ln -sfv $in $out
     done
 }
 
 # vscode
-function install_vscode(){
-    export VS_DIR
-    VS_DIR="~/.config/Code"
-    for f in $(fd . vscode); do
-        in="$DIR/$f"
-        out="$VS_DIR/${f##*/}"
-        echo "ln -sfv "$in $out""
+function vscode_cfg(){
+    # config files
+    VS_DIR=$(echo ~/.config/Code)
+    VS_DATA="vscode/data"
+    for f in $(fd . -t f $VS_DATA); do
+        in=$DIR/$f
+        out=$VS_DIR/${f/$VS_DATA\/}
+        ln -sfv $in $out
     done
 }
 
-install_vscode
+function vscode_ext(){
+    # extensions
+    VS_EXT="vscode/extensions"
+    diff $VS_EXT <(code --list-extensions) | \
+
+    while read c; do
+
+        mode=${c:0:1}
+        ext=${c:2}
+
+        # install
+        if [[ $mode == '<' ]]; then
+            code --install-extension $ext
+        # uninstall
+        elif [[ $mode == '>' ]]; then
+            code --uninstall-extension $ext
+        fi
+    done
+}
+
+bash_cfg
+vscode_cfg
+vscode_ext
