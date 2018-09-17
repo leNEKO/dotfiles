53bd0f31db1400ef2a5df85b52b33ccb0da3f357
neko <le.neko@gmail.com>
2018-09-17 22:24:17 +0200

    vs code dot files

--- install.sh
+++ install.sh
@@ -5,7 +5,23 @@ export DIR
 DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # bash
-for in in bash/*; do
-    out=~/.${in##*/}
-    ln -sfv "$DIR/$in" "$out"
-done
+function install_bash(){
+    for f in $(fd . bash); do
+        in="$DIR/$f"
+        out="~/.${f##*/}"
+        ln -sfv "$in $out"
+    done
+}
+
+# vscode
+function install_vscode(){
+    export VS_DIR
+    VS_DIR="~/.config/Code"
+    for f in $(fd . vscode); do
+        in="$DIR/$f"
+        out="$VS_DIR/${f##*/}"
+        echo "ln -sfv "$in $out""
+    done
+}
+
+install_vscode
